5e7269103ed9abfc46b9fae3a150c0c67daa0ab8
João Bosco <ti.junior@gmail.com>
2022-09-28 22:27:56 -0300

    validação do cadastro de usuário.

--- app/src/main/java/com/example/olxclone/ui/register/SignUpActivity.java
+++ app/src/main/java/com/example/olxclone/ui/register/SignUpActivity.java
@@ -1,13 +1,15 @@
 package com.example.olxclone.ui.register;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.olxclone.databinding.ActivitySignUpBinding;
 
 /**
- *  Created by João Bosco on 21/09/2022.
+ * Created by João Bosco on 21/09/2022.
  */
 public class SignUpActivity extends AppCompatActivity {
 
@@ -19,5 +21,38 @@ public class SignUpActivity extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+    }
+
+    public void validate(View view) {
+
+        String name = binding.editName.getText().toString();
+        String email = binding.editEmail.getText().toString();
+        String phone = binding.editPhone.getText().toString();
+        String password = binding.editPassword.getText().toString();
+
+        if (!name.isEmpty()) {
+            if (!email.isEmpty()) {
+                if (!phone.isEmpty()) {
+                    if (!password.isEmpty()) {
+
+                        Toast.makeText(this, "Tudo certo...", Toast.LENGTH_LONG).show();
+
+                    } else {
+                        binding.editPassword.requestFocus();
+                        binding.editPassword.setError("Informe sua senha!");
+                    }
+                } else {
+                    binding.editPhone.requestFocus();
+                    binding.editPhone.setError("Informe seu telefone!");
+                }
+            } else {
+                binding.editEmail.requestFocus();
+                binding.editEmail.setError("Informe seu e-mail!");
+            }
+        } else {
+            binding.editName.requestFocus();
+            binding.editName.setError("Informe seu nome!");
+        }
     }
 }
