1a7344ce9c88546e113e471b9c5332b986f2222c
João Bosco <ti.junior@gmail.com>
2022-10-10 23:06:47 -0300

    implementação do BottomNavigationView.

--- app/src/main/java/com/example/olxclone/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/olxclone/ui/activity/MainActivity.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.olxclone.R;
+import com.example.olxclone.databinding.ActivityMainBinding;
 
 /**
  *  Created by João Bosco on 21/09/2022.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
